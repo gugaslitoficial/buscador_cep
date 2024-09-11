44df1999dc3fb41a5e27a8d614f46964cd7da6cb
gugaslitoficial <gustavo-romao@hotmail.com>
2024-09-11 13:34:21 -0300

    Responsividade

--- src/components/cepSearch.tsx
+++ src/components/cepSearch.tsx
@@ -32,23 +32,23 @@ import React, { useState } from 'react';
       };
 
     return (
-        <div className="flex flex-col items-center p-5 w-1/2">
-        <h1 className='text-white font-bold text-5xl mb-20'>Buscador de CEP:</h1>
+        <div className="flex flex-col items-center p-1 sm:p-5 w-full max-w-full md:w-4/5 lg:w-3/4 xl:w-1/2">
+        <h1 className='text-white font-bold text-xl sm:text-4xl md:text-4xl xl:text-5xl 2xl:text-6xl mb-10 md:mb-16 xl:mb-20'>Buscador de CEP:</h1>
         <input
             type="text"
-            className="p-4 border-green-600 border-2 rounded-xl mb-6 w-full"
+            className="p-3 md:p-3 2xl:p-5 border-green-600 border-2 rounded-xl mb-4 md:mb-7 lg:mb-8 xl:mb-10 w-full"
             placeholder="Digite o CEP"
             value={cep}
             onChange={(e) => setCep(e.target.value)}
         />
         <button
             onClick={handleSearch}
-            className="bg-green-600 text-white p-3 rounded hover:bg-green-800 text-base font-semibold"
+            className="bg-green-600 text-white p-2 md:p-3 2xl:p-4 rounded hover:bg-green-800 text-base font-semibold"
         >
             Buscar
         </button>
         {result && (
-            <div className="mt-10 p-1 border-green-600 border-4 rounded text-white w-1/2">
+            <div className="mt-8 md:mt-10 p-2 border-green-600 border-4 rounded text-white w-full sm:w-3/4 lg:w-1/2 xl:w-2/3 2xl:w-1/2 text-center text-sm md:text-base xl:text-lg">
             {result.erro ? (
               <p>CEP não encontrado.</p>
             ) : (
